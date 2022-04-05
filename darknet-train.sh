5982f4371d054d02bd2a2f7f323351708f715a80
Lucas Prone <lucas.prone@globant.com>
2022-04-05 14:55:00 -0300

    fix path

--- darknet-train.sh
+++ darknet-train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./darknet detector train \
-  /tmp/iaia-mario/iaia-mario.data \
-  /tmp/iaia-mario/yolov3-tiny-iaia-mario.cfg
+/content/darknet/darknet detector train \
+  /content/iaia-mario/iaia-mario.data \
+  /content/iaia-mario/yolov3-tiny-iaia-mario.cfg
